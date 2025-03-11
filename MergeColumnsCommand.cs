3159dc3732325f8f42996e9fecaf9c10104f0b9f
Asriel Camora <asriel@camora.dev>
2025-03-11 12:53:57 -0700

    Add merge-columns command

---
+++ MergeColumnsCommand.cs
@@ -0,0 +1,49 @@
+using DotMake.CommandLine;
+
+namespace EXDTooler;
+
+[CliCommand(Parent = typeof(MainCommand))]
+public sealed class MergeColumnsCommand
+{
+    public required MainCommand Parent { get; set; }
+
+    [CliOption(Required = true, Description = "Path to base directory with folders.", ValidationRules = CliValidationRules.ExistingDirectory)]
+    public required string BasePath { get; set; }
+
+    [CliOption(Required = false, Description = "Path to columns.yml file within a BasePath subfolder.")]
+    public string ColumnsPath { get; set; } = ".github/columns.yml";
+
+    public Task RunAsync()
+    {
+        var token = Parent.Init();
+
+        var files = Directory.GetDirectories(BasePath).Select(p => (Version: Path.GetFileName(p), Path: Path.Combine(p, ColumnsPath))).OrderByDescending(f => f.Version).ToArray();
+
+        ColDefReader? baseSheets = null;
+        Dictionary<(string Sheet, uint Hash), string> latestFiles = [];
+        foreach (var file in files)
+        {
+            var sheets = ColDefReader.FromColumnFile(file.Path);
+
+            Log.Info($"Version: {file.Version}");
+            Log.Info($"Hash: {sheets.HashString}");
+
+            foreach (var sheet in sheets.Sheets)
+            {
+                var hash = sheets.GetColumnsHash(sheet.Key);
+                if (latestFiles.TryGetValue((sheet.Key, hash), out var latestVersion))
+                {
+                    var latestSheet = Path.Combine(BasePath, latestVersion, $"{sheet.Key}.yml");
+                    var oldSheet = Path.Combine(BasePath, file.Version, $"{sheet.Key}.yml");
+                    File.Copy(latestSheet, oldSheet, true);
+                }
+                else
+                    latestFiles.Add((sheet.Key, hash), file.Version);
+            }
+
+            baseSheets = sheets;
+        }
+
+        return Task.CompletedTask;
+    }
+}
